49fe8fc024d8839d1dfcdee9195f5c695bb7106e
kgmahendran <mahivalarmathi1@gmail.com>
2021-03-10 11:25:42 +0530

    Shared lib

--- vars/GitClone.groovy
+++ vars/GitClone.groovy
@@ -2,7 +2,7 @@
 
 def call(Map getval) {
 	checkout([$class: 'GitSCM', 
-	branches: [[ name: getval.branches ]], 
+	branches: [[ name: getval.branch ]], 
 	userRemoteConfigs: [[credentialsId: '12afdeaa-e9f3-4be8-bc17-64ccba068dbe', url: getval.url ]]
 	])
   }
