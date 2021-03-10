63db4cdd1b5791582dbbb628e3f3891a439a8fc1
kgmahendran <mahivalarmathi1@gmail.com>
2021-03-10 17:55:42 +0530

    ArtifactoryUpload.groovy

--- vars/ArtifactoryUpload.groovy
+++ vars/ArtifactoryUpload.groovy
@@ -1,16 +1,16 @@
 #!/usr/bin/env groovy
 
-def call (String ARTIFACTORY_NAME, String REPO_NAME, String JOB_NAME, int  BUILD_NUMBER) {
-	rtUpload (
-		serverId: ${ARTIFACTORY_NAME},
-			spec: '''{
-				"files": [
-					{
-					"pattern": "target/*.war",
-					"target": "${REPO_NAME}/${JOB_NAME}/${BUILD_NUMBER}/"
-				}
-			]
-		}''',
-	buildName: ${JOB_NAME}
-	)
+def artifactory_call () {
+				rtUpload (
+					serverId: 'central',
+					spec: '''{
+						"files": [
+										{
+										"pattern": "target/*.war",
+										"target": "Demo-Upload/$JOB_NAME/$BUILD_NUMBER/"
+										}
+									]
+								}''',
+								buildName: 'Pipeline-Groovy'
+							)
 }
