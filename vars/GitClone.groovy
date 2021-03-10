23476fa8b3987b8e96cdeeac17a16d064b5155bf
kgmahendran <mahivalarmathi1@gmail.com>
2021-03-10 11:15:01 +0530

    Git SCM

--- vars/GitClone.groovy
+++ vars/GitClone.groovy
@@ -2,6 +2,7 @@
 
 def call(Map getval) {
 	checkout([$class: 'GitSCM', 
-	branches: [[name: 'getval.branches']], 
-	userRemoteConfigs: [[credentialsId: '12afdeaa-e9f3-4be8-bc17-64ccba068dbe', url: 'getval.url']]])
+	branches: [[ name: getval.branches ]], 
+	userRemoteConfigs: [[credentialsId: '12afdeaa-e9f3-4be8-bc17-64ccba068dbe', url: getval.url ]]
+	])
   }
