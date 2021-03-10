ceaadd70d59e9efebbc02c6bc5860d3a232512e1
kgmahendran <mahivalarmathi1@gmail.com>
2021-03-10 11:06:28 +0530

    git checkout

--- vars/GitClone.groovy
+++ vars/GitClone.groovy
@@ -1,11 +1,5 @@
 #!/usr/bin/env groovy
 
 def call(Map stageParams) {
-
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
-		credentialsId: '12afdeaa-e9f3-4be8-bc17-64ccba068dbe'
-    ])
+	 git branch: 'stageParams.branch', credentialsId: '12afdeaa-e9f3-4be8-bc17-64ccba068dbe', url: 'stageParams.url '
   }
