8634d6148c15d49a67120ea65fe32d080c0d7ddb
kgmahendran <mahivalarmathi1@gmail.com>
2021-03-17 19:32:09 +0530

    Test

--- vars/CSVReadValues.groovy
+++ vars/CSVReadValues.groovy
@@ -10,9 +10,8 @@ println CSVfilecontent.text
 println "############  List ################"
 
 def records = readCSV file: 'D:/Demo-Pipeline/CSV-Jenkins/input.csv'
-assert records[0][0] == 'key'
-assert records[1][1] == 'b'
 
-println "$key $b $records[0][0]"
+
+println "$records[0][0]"
 
 }
