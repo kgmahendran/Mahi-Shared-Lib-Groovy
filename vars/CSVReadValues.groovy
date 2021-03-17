97a59be5c334fb553a7368649d7528f3ede50636
kgmahendran <mahivalarmathi1@gmail.com>
2021-03-17 17:51:05 +0530

    Test

--- vars/CSVReadValues.groovy
+++ vars/CSVReadValues.groovy
@@ -1,41 +1,10 @@
-import java.io.*;
-import java.util.*;
+def call()
+{
 
-public class CsvParser {
+def csvfilepath="D:/Demo-Pipeline/CSV-Jenkins/input.csv"
 
-    public static void main(String[] args) {
-        String fileName= "C:/Program Files (x86)/Jenkins/workspace/Pipeline-CSVReader/input.csv";
-        File file= new File(fileName);
+File CSVfilecontent=new File(csvfilepath)
 
-        // this gives you a 2-dimensional array of strings
-        List<List<String>> lines = new ArrayList<>();
-        Scanner inputStream;
-
-        try{
-            inputStream = new Scanner(file);
-
-            while(inputStream.hasNext()){
-                String line= inputStream.next();
-                String[] values = line.split(",");
-                // this adds the currently parsed line to the 2-dimensional string array
-                lines.add(Arrays.asList(values));
-            }
-
-            inputStream.close();
-        }catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        // the following code lets you iterate through the 2-dimensional array
-        int lineNo = 1;
-        for(List<String> line: lines) {
-            int columnNo = 1;
-            for (String value: line) {
-                System.out.println("Line " + lineNo + " Column " + columnNo + ": " + value);
-                columnNo++;
-            }
-            lineNo++;
-        }
-    }
+println CSVfilecontent.text
 
 }
