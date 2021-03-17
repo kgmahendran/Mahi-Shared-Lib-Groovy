1b578e16e2b7b228e712aeac08df29e560a2f567
kgmahendran <mahivalarmathi1@gmail.com>
2021-03-17 20:29:17 +0530

    Test

--- vars/CSVReadValues.groovy
+++ vars/CSVReadValues.groovy
@@ -13,4 +13,12 @@ def records = readCSV file: 'D:/Demo-Pipeline/CSV-Jenkins/input.csv'
 
 
 println "$records"
+
+println "-----------------------------------------"
+
+def record0= records[0].CSVRecord 
+def record1= records[1].CSVRecord 
+
+println "$record0"
+println "$record1"
 }
