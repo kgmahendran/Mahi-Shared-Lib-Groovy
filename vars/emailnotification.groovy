3d311de76a0cbe353b451b377cc391dae92ac5ba
kgmahendran <mahivalarmathi1@gmail.com>
2021-03-18 19:49:32 +0530

    test

--- vars/emailnotification.groovy
+++ vars/emailnotification.groovy
@@ -1,4 +1,4 @@
 def call()
 {
-emailext body: '************Test###', subject: 'Test###', to: 'mahivalarmathi1@gmail.com'
+emailext body: '${JELLY_SCRIPT,template="html"}', subject: '[Jenkins_Pipeline]::[CSV Read] #BuildNumber : $BUILD_NUMBER', to: 'mahivalarmathi1@gmail.com'
 }
