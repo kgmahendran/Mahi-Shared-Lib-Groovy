65a3efb414d7f409eaba7c4180c185bbb980fee6
kgmahendran <mahivalarmathi1@gmail.com>
2021-03-18 09:04:04 +0530

    Test

--- vars/CSVReadValues.groovy
+++ vars/CSVReadValues.groovy
@@ -9,9 +9,18 @@ println CSVfilecontent.text
 
 println "############  List ################"
 
-String[][] records=readCSV file: 'D:/Demo-Pipeline/CSV-Jenkins/input.csv', text: ''
+Reader in = new FileReader("D:/Demo-Pipeline/CSV-Jenkins/input.csv");
+Iterable<CSVRecord> records = CSVFormat.DEFAULT.parse(in);
+for (CSVRecord record : records) {
+    String columnOne = record.get(0);
+    String columnTwo = record.get(1);
+	String columnThree = record.get(2);
+	String columnFour= record.get(3);
+	String columnFive = record.get(4);
+}
 
-println "$records.getRecords().get(0)"
+println "$record.get(0)"
+
 
 }
 
