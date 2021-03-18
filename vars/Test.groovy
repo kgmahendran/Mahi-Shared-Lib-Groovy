98722b526120b33f7e83ab2ccf5ac20d53e2e465
kgmahendran <mahivalarmathi1@gmail.com>
2021-03-18 20:36:14 +0530

    Test.groovy

--- vars/Test.groovy
+++ vars/Test.groovy
@@ -1,7 +1,18 @@
 def call()
 {
 
-def records = readCSV file: 'D:\\Demo-Pipeline\\CSV-Jenkins\\Input.csv'
-println "$records"
+CSVFormat csvFormat = CSVFormat.DEFAULT.withHeader();
+try (CSVParser csvParser = csvFormat.parse(Files.newBufferedReader(Paths.get("D:\\Demo-Pipeline\\CSV-Jenkins\\Input.csv")))) {
+    System.out.println(csvParser.getHeaderNames());
+    for (CSVRecord csvRecord : csvParser) {
+        System.out.println(csvRecord);
+        System.out.println("  toMap(): " + csvRecord.toMap());
+
+        Map<String, String> map = csvRecord.toMap();
+        for (String name : csvParser.getHeaderNames())
+            map.putIfAbsent(name, "");
+        System.out.println("  Values  : " + map);
+    }
+}
 
 }
