6b133ce2b79bba1d60bd049f7f8673b0e2b6a955
kgmahendran <mahivalarmathi1@gmail.com>
2021-03-18 08:20:38 +0530

    Test

--- vars/CSVReadValues.groovy
+++ vars/CSVReadValues.groovy
@@ -1,12 +1,3 @@
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 def call()
 {
 
@@ -18,30 +9,10 @@ println CSVfilecontent.text
 
 println "############  List ################"
 
-def SAMPLE_CSV_FILE_PATH="D:/Demo-Pipeline/CSV-Jenkins/input.csv"
-            Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
-            CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT
-                    .withFirstRecordAsHeader()
-                    .withIgnoreHeaderCase()
-                    .withTrim());
+String[][] records=readCSV file: 'D:/Demo-Pipeline/CSV-Jenkins/input.csv', text: ''
 
-for (CSVRecord csvRecord : csvParser) {
-                // Accessing values by Header names
-                def AppID = csvRecord.get("AppID");
-                String AppName = csvRecord.get("AppName");
-                String Environment = csvRecord.get("Environment");
-                def  ReleaseVersion = csvRecord.get("ReleaseVersion");
-				def Status = csvRecord.get("Status");
+println records
 
-                System.out.println("Record No - " + csvRecord.getRecordNumber());
-                System.out.println("---------------");
-                System.out.println("AppID : " + AppID);
-                System.out.println("AppName : " + AppName);
-                System.out.println("Environment : " + Environment);
-                System.out.println("ReleaseVersion : " + ReleaseVersion);
-				System.out.println("Status : " + Status);
-                System.out.println("---------------\n\n");
-            }
 }
 
 
