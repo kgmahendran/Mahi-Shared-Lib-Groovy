8a158cd6afc4b1fe5d990c3aff0150ad8c3f65e6
kgmahendran <mahivalarmathi1@gmail.com>
2021-03-18 22:06:26 +0530

    Test

--- vars/CSVReadValues.groovy
+++ vars/CSVReadValues.groovy
@@ -1,4 +1,3 @@
-import BuildDetails
 import java.io.BufferedReader
 import java.util.function.Function
 import java.util.regex.Pattern
@@ -8,6 +7,57 @@ import java.io.*
 import java.util.List
 @NonCPS
 def call() {
+class BuildDetails {
+	def appId;
+	def appName;
+	def environment;
+	def releaseVersion;
+	def status;
+	BuildDetails() {}
+	BuildDetails(def appId, def appName, def environment, def releaseVersion, def status) {
+		super();
+		this.appId = appId;
+		this.appName = appName;
+		this.environment = environment;
+		this.releaseVersion = releaseVersion;
+		this.status = status;
+	}
+	int getAppId() {
+		return appId;
+	}
+	void setAppId(int appId) {
+		this.appId = appId;
+	}
+	def  getAppName() {
+		return appName;
+	}
+	def setAppName(String appName) {
+		this.appName = appName;
+	}
+	def getEnvironment() {
+		return environment;
+	}
+	def setEnvironment(String environment) {
+		this.environment = environment;
+	}
+	def getReleaseVersion() {
+		return releaseVersion;
+	}
+	def setReleaseVersion(String releaseVersion) {
+		this.releaseVersion = releaseVersion;
+	}
+	def getStatus() {
+		return status;
+	}
+	def setStatus(String status) {
+		this.status = status;
+	}
+
+	def groupBy() {
+		return getAppId() + "-" + getAppName() + "-" + getReleaseVersion();
+	}
+}
+
 
 
 def BuildDet = new BuildDetails()
