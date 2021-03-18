bcd7c06b1435189e75417206fb2f8a34226c9e59
kgmahendran <mahivalarmathi1@gmail.com>
2021-03-18 21:34:28 +0530

    Test.groovy

--- vars/Test.groovy
+++ vars/Test.groovy
@@ -6,8 +6,7 @@ def call()
 Reader filereader=new FileReader("D:\\Demo-Pipeline\\CSV-Jenkins\\Input.csv")	
 Iterable<CSVRecord> records = CSVFormat.DEFAULT.parse(filereader);
     for (CSVRecord csvRecord : records) {
-		def APPID=csvRecord.get(0)
-        println "$APPID"
+        println "${csvRecord.get(0)} ${csvRecord.get(1)} ${csvRecord.get(2)} ${csvRecord.get(3)} ${csvRecord.get(4)}"
     }
 
 }
