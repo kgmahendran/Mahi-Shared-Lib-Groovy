61d0bba43181dd082e3e982739fb7753161d85c7
kgmahendran <mahivalarmathi1@gmail.com>
2021-03-19 16:12:32 +0530

    test

--- vars/Test.groovy
+++ vars/Test.groovy
@@ -17,7 +17,7 @@ Iterable<CSVRecord> records = CSVFormat.DEFAULT
 
 Map<String, List<CSVRecord>> recordListBySK =   StreamSupport
 		.stream(records.spliterator(), false).
-		collect(Collectors.groupingBy({it.AppID},{it.AppName},{it.ReleaseVersion} ));
+		collect(Collectors.groupingBy({record -> record.get("AppID")} ));
 		
 
 recordListBySK.each { key,value ->
