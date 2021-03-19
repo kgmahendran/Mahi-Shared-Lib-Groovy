012eda08cf5f40343d2757141e9ee4870d9969e1
kgmahendran <mahivalarmathi1@gmail.com>
2021-03-19 15:53:21 +0530

    Test.groovy

--- vars/Test.groovy
+++ vars/Test.groovy
@@ -1,58 +1,5 @@
 import org.apache.commons.csv.*
 import java.util.stream.*
-
-class BuildDetails {
-	def appId;
-	def appName;
-	def environment;
-	def releaseVersion;
-	def status;
-	BuildDetails() {}
-	BuildDetails(def appId, def appName, def environment, def releaseVersion, def status) {
-		super();
-		this.appId = appId;
-		this.appName = appName;
-		this.environment = environment;
-		this.releaseVersion = releaseVersion;
-		this.status = status;
-	}
-	int getAppId() {
-		return appId;
-	}
-	void setAppId(int appId) {
-		this.appId = appId;
-	}
-	def  getAppName() {
-		return appName;
-	}
-	def setAppName(String appName) {
-		this.appName = appName;
-	}
-	def getEnvironment() {
-		return environment;
-	}
-	def setEnvironment(String environment) {
-		this.environment = environment;
-	}
-	def getReleaseVersion() {
-		return releaseVersion;
-	}
-	def setReleaseVersion(String releaseVersion) {
-		this.releaseVersion = releaseVersion;
-	}
-	def getStatus() {
-		return status;
-	}
-	def setStatus(String status) {
-		this.status = status;
-	}
-	
-	def getCsvfilterval() {
-		return getAppId() + "-" + getAppName() + "-" + getReleaseVersion();
-	}
-}
-
-
 @NonCPS
 def call()
 {
@@ -70,9 +17,11 @@ Iterable<CSVRecord> records = CSVFormat.DEFAULT
 
 Map<String, List<CSVRecord>> recordListBySK =   StreamSupport
 		.stream(records.spliterator(), false).
-		collect(Collectors.groupingBy(BuildDetails.&getCsvfilterval as Function);
+		collect(Collectors.groupingBy({record -> record.get("AppID")} ));
 		
 
+recordListBySK.each { key,value ->
+		println "$key : $value"
 }
 
 }
