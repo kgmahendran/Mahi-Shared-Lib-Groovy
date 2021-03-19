5a216a5ddc2c484161b96a2fa3f93e85978919b0
kgmahendran <mahivalarmathi1@gmail.com>
2021-03-19 09:10:11 +0530

    Test

--- vars/CSVReadValues.groovy
+++ vars/CSVReadValues.groovy
@@ -53,7 +53,7 @@ class BuildDetails {
 		this.status = status;
 	}
 
-	def groupBy1() {
+	 def groupcsvvalues() {
 		return getAppId() + "-" + getAppName() + "-" + getReleaseVersion();
 	}
 }
@@ -73,7 +73,7 @@ List<BuildDetails> buildStatus = filecontent.lines().skip(1).map({m ->
 StringBuilder sb = new StringBuilder();
 sb.append(header);
 Map<String, List<BuildDetails>> buildStatusMap = buildStatus.stream()
-.collect(Collectors.groupingBy(BuildDetails .&groupBy1 as Function));
+.collect(Collectors.groupingBy(BuildDetails .&groupcsvvalues as Function));
 for (Map.Entry<String, List<BuildDetails>> entry : buildStatusMap.entrySet()) {
 	System.out.println(entry.getKey());
 	List<BuildDetails> buildList = entry.getValue();
