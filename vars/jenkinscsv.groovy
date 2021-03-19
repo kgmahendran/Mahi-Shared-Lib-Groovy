5551719ebc671e7b3a8af8fdf481de8fefea0a33
kgmahendran <mahivalarmathi1@gmail.com>
2021-03-19 11:20:45 +0530

    test

--- vars/jenkinscsv.groovy
+++ vars/jenkinscsv.groovy
@@ -62,7 +62,7 @@ def buildDetails = new BuildDetails();
 String header = "App ID,App Name,Release Version,Environments Passed,Environment Failed,Comments";
 Pattern pattern = Pattern.compile(",");
 
-BufferedReader filecontent = new BufferedReader(new FileReader("$WORKSPACE/Pipeline-CSVReader/Input.csv"));
+BufferedReader filecontent = new BufferedReader(new FileReader("C:\\Program Files (x86)\\Jenkins\\workspace\\Pipeline-CSVReader\\Input.csv"));
 List<BuildDetails> buildStatus = filecontent.lines().skip(1).map({m ->
 				String[] x = pattern.split(m);
 				println "${x[0]}, ${x[1]}, ${x[2]}, ${x[3]}, ${x[4]}"
@@ -106,7 +106,7 @@ for (Map.Entry<String, List<BuildDetails>> entry : buildStatusMap.entrySet()) {
 }
 PrintWriter writer;
 try {
-	writer = new PrintWriter(new File("$WORKSPACE/Pipeline-CSVReader/Input.csv"))
+	writer = new PrintWriter(new File("C:\\Program Files (x86)\\Jenkins\\workspace\\Pipeline-CSVReader\\Release_Status.csv"))
 	writer.write(sb.toString());
 	System.out.println("done!");
 
