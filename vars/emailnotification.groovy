895c806a407bf379e4e095e2a154d42530a57c5e
kgmahendran <mahivalarmathi1@gmail.com>
2021-03-19 17:14:50 +0530

    test

--- vars/emailnotification.groovy
+++ vars/emailnotification.groovy
@@ -1,4 +1,7 @@
 def call()
 {
-emailext attachLog: true, attachmentsPattern: 'Release_Status.csv', body: '${JELLY_SCRIPT,template="html-with-health-and-console"}', subject: '[Jenkins_Pipeline]::[CSV Read] #BuildNumber : $BUILD_NUMBER #Status : $BUILD_STATUS', to: 'mahivalarmathi1@gmail.com,mahendran.irusappan@cognizant.com'
+emailext attachLog: true, attachmentsPattern: 'Release_Status.csv', 
+body: '${SCRIPT, template="groovy-html.template"}', 
+subject: '[Jenkins_Pipeline]::[CSV Read] #BuildNumber : $BUILD_NUMBER #Status : $BUILD_STATUS', 
+to: 'mahivalarmathi1@gmail.com,mahendran.irusappan@cognizant.com'
 }
