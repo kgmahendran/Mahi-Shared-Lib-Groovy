2cfd49add76199806f41656398218a7470c3d886
kgmahendran <mahivalarmathi1@gmail.com>
2021-03-19 16:30:52 +0530

    Test.groovy

--- vars/Test.groovy
+++ vars/Test.groovy
@@ -15,13 +15,24 @@ Iterable<CSVRecord> records = CSVFormat.DEFAULT
 		.parse(filereader);
 
 
-Map<String, List<CSVRecord>> recordListBySK =   StreamSupport
+Map<String, List<CSVRecord>> recordFiltered =   StreamSupport
 		.stream(records.spliterator(), false).
 		collect(Collectors.groupingBy({record -> record.get("AppID")+"-"+record.get("AppName")+"-"+record.get("ReleaseVersion")} ));
 		
 
-recordListBySK.each { key,value ->
+recordFiltered.each { key,value ->
 		println "$key : $value"
+}
+
+for (Map.Entry<String, List<CSVRecord>> entry : recordFiltered.entrySet()) {
+	System.out.println(entry.getKey());
+	List<CSVRecord> buildList = entry.getValue();
+	
+	List<CSVRecord> failedList = buildList.stream().filter({f -> f.get("Status").contains("Failed")})
+	.collect(Collectors.toList());
+	
+	println "$failedList"
+
 }
 
 }
