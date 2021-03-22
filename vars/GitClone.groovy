a1476cc25d3bc69f9b7a3e4db3fc0201d1c968e7
Mahendran.Irusappan <javadevopsmc50@iiht.tech>
2021-03-22 11:05:00 +0530

    Mahi added

--- vars/GitClone.groovy
+++ vars/GitClone.groovy
@@ -3,6 +3,7 @@
 def call(Map getval) {
 	checkout([$class: 'GitSCM', 
 	branches: [[ name: getval.branch ]], 
-	userRemoteConfigs: [[credentialsId: '12afdeaa-e9f3-4be8-bc17-64ccba068dbe', url: getval.url ]]
+	userRemoteConfigs: [[credentialsId: 'Pipeline', url: getval.url ]]
 	])
   }
+//12afdeaa-e9f3-4be8-bc17-64ccba068dbe
