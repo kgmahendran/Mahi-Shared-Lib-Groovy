67ea149ad5a640c4b51856d2c50aa5d0ff95183d
kgmahendran <mahivalarmathi1@gmail.com>
2021-03-25 20:04:48 +0530

    Test.groovy

--- vars/Test.groovy
+++ vars/Test.groovy
@@ -2,7 +2,7 @@ def call() {
 def records=readCSV file: 'D:\\Demo-Pipeline\\CSV-Jenkins\\Input.csv', text: ''
 println "$records"
 println"++++++++++++++++++++++++++++"
-print"$records.get("AppID")"
+//print"$records.get("AppID")"
 println"++++++++++++++++++++++++++++"
 print"$records.get(0)"
 }
