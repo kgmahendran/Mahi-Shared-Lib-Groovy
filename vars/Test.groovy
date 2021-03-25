96e6e6fb5399f488fdceb1714d8c51b9ce16ed71
kgmahendran <mahivalarmathi1@gmail.com>
2021-03-25 20:31:07 +0530

    test

--- vars/Test.groovy
+++ vars/Test.groovy
@@ -1,4 +1,5 @@
 import org.apache.commons.csv.*
+import java.util.*
 def call() {
 def HEADERS = ["AppID","AppName","Environment","ReleaseVersion","Status"]
 def records_values=readCSV file: 'D:\\Demo-Pipeline\\CSV-Jenkins\\Input.csv'
