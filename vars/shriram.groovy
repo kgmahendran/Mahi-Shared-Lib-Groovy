a2b9dc8302d96ddae3209941345983e9bb908e12
kgmahendran <mahivalarmathi1@gmail.com>
2021-04-13 12:57:43 +0530

    test

--- vars/shriram.groovy
+++ vars/shriram.groovy
@@ -2,19 +2,10 @@
 import org.apache.commons.csv.*
 import org.jenkinsci.plugins.*
 import java.util.stream.*
-@NonCPS
 def call() {
 
 
 def records = readCSV file: 'D:\\Demo-Pipeline\\CSV-Jenkins\\Input.csv' , format: CSVFormat.DEFAULT.withHeader().withFirstRecordAsHeader()
 
-Map<String, List<CSVRecord>> recordFiltered =   StreamSupport
-		.stream(records.spliterator(), false).
-		collect(Collectors.groupingBy({record -> record.get("AppID")+"-"+record.get("AppName")+"-"+record.get("ReleaseVersion")} ));
-		
-println("**Read the content and Filterted using #AppID,#AppName and #Release Version**")
-println("-----------------------------------------")
-recordFiltered.each { key,value ->
-		println "$key : $value"
-}
+println "$records"
   }
