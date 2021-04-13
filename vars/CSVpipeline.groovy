9732ddbfc04d969a39fe2e08de37f0a1ac26aa02
kgmahendran <mahivalarmathi1@gmail.com>
2021-04-13 19:26:59 +0530

    test

--- vars/CSVpipeline.groovy
+++ vars/CSVpipeline.groovy
@@ -1,39 +1,42 @@
 #!/usr/bin/env groovy
 import org.apache.commons.csv.*
 import org.jenkinsci.plugins.*
+import java.util.stream.*
 
 def call() {
 
-	def records = readCSV file: '$WORKSPACE\\Input.csv' , format: CSVFormat.DEFAULT.withHeader().withFirstRecordAsHeader()
-	def result = records.groupBy({record -> record.get("AppID")+"-"+record.get("AppName")+"-"+record.get("ReleaseVersion")})
 	def Output_Headers="AppID,AppName,ReleaseVersion,EnvironmentsPassed,EnvironmentFailed,Comments"
+	def records = readCSV file: 'D:\\Demo-Pipeline\\CSV-Jenkins\\Input.csv' , format: CSVFormat.DEFAULT.withHeader().withFirstRecordAsHeader()
+	def result = records.groupBy({record -> record.get("AppID")+"-"+record.get("AppName")+"-"+record.get("ReleaseVersion")})
 
-	File file = new File("$WORKSPACE\\output.csv")
+	File file = new File("D:\\Demo-Pipeline\\CSV-Jenkins\\output.csv")
+	file.text = ''
 	file.append(Output_Headers)
 
-	List<CSVRecord> Status_Passed;
-	List<CSVRecord> Status_Failed;
+	List<CSVRecord> PassedList;
+	List<CSVRecord> FailedList;
 	def Spliter=","
 	def Comments="NA";
 	for (Map.Entry<String, List<CSVRecord>> entry : result.entrySet()) {
-		def Failed_Env="NA";
-		def Passed_Env="NA";
+		def failedEnvList="NA";
+		def passedEnvList="NA";
 		def  buildList = entry.getValue();
-		Status_Passed= buildList.findAll{f -> !f.get("Status").contains("Failed")}
-		Status_Failed =  buildList.findAll{it.Status.contains("Failed")}
+		PassedList= buildList.findAll{f -> !f.get("Status").contains("Failed")}
+		FailedList =  buildList.findAll{it.Status.contains("Failed")}
 
-		if (Status_Passed != null && Status_Passed.size() > 0) {
-			def arr_val = Status_Passed.collect{it.Environment}
-			Passed_Env = arr_val.join('|')
+		if (PassedList != null && PassedList.size() > 0) {
+			def arrr = PassedList.collect{it.Environment}
+			passedEnvList = arrr.join('|')
 			Comments="Passed";
 		}
-		if (Status_Failed != null && Status_Failed.size() > 0) {
-			def arr_val1 = Status_Failed.collect{it.Environment}
-			Failed_Env = arr_val1.join('|')
+		
+		if (FailedList != null && FailedList.size() > 0) {
+			def arrr1 = FailedList.collect{it.Environment}
+			failedEnvList = arrr1.join('|')
 			Comments="Failed";
 		}
 
 		file.append("\n")
-		file.append(buildList.get(0).get("AppID") + Spliter + (buildList.get(0).get("AppName")) + Spliter + buildList.get(0).get("ReleaseVersion") + Spliter + Passed_Env + Spliter +Failed_Env + Spliter + Comments)
+		file.append(buildList.get(0).get("AppID") + Spliter + (buildList.get(0).get("AppName")) + Spliter + buildList.get(0).get("ReleaseVersion") + Spliter + passedEnvList + Spliter +failedEnvList + Spliter + Comments)
 	}
 }
