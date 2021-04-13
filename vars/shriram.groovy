22331ab7dd11b2285e1f36bc5898a2af4558eb4e
kgmahendran <mahivalarmathi1@gmail.com>
2021-04-13 16:22:37 +0530

    Test

--- vars/shriram.groovy
+++ vars/shriram.groovy
@@ -27,11 +27,19 @@ println "********************************"
 for (Map.Entry<String, List<CSVRecord>> entry : result.entrySet()) {
 
 	def  buildList = entry.getValue();
-	def PasedList= buildList.findAll{f -> !f.get("Status").contains("Failed")}.collect{it.Status}
+	def PassedList= buildList.findAll{f -> !f.get("Status").contains("Failed")}.collect{it.Status}
 	def FailedList =  buildList.findAll{it.Status.contains("Failed")}.collect{it.Status}
+	def passedEnvList=NA
+	def failedEnvList=NA
+if (PassedList != null && PassedList.size() > 0) {
+		passedEnvList = PassedList.get("Environment").join('|')
+	}
+	if (FailedList != null && FailedList.size() > 0) {
+		failedEnvList = FailedList.get("Status").join('|')
+	}
 	def FFF=FailedList.join('|')
-	println"$FFF"
-	println "$PasedList"
+	println"$PassedList"
+	println "$FailedList"
 	
 	//println "$failedEnvList"
 
