e223ea30fce2b6cd2d2bd40fba864c4e0dfff417
kgmahendran <mahivalarmathi1@gmail.com>
2021-04-13 13:08:17 +0530

    test

--- vars/shriram.groovy
+++ vars/shriram.groovy
@@ -10,4 +10,14 @@ def records = readCSV file: 'D:\\Demo-Pipeline\\CSV-Jenkins\\Input.csv' , format
 records.each { key,value ->
 		println "$key : $value"
 }
+
+println "++++++++++++++++++++++++++++++++++++++++"
+
+def result = records.groupBy({it.AppID}, {it.ReleaseVersion})
+
+println "++++++++++++++++++++++++++++++++++++++++"
+
+result.each { key,value ->
+		println "$key : $value"
+}
   }
