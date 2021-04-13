1036919ee3eae030f7cafe0b2438b378a546c6d6
kgmahendran <mahivalarmathi1@gmail.com>
2021-04-13 18:34:29 +0530

    test

--- vars/shriram.groovy
+++ vars/shriram.groovy
@@ -57,7 +57,7 @@ for (Map.Entry<String, List<CSVRecord>> entry : result.entrySet()) {
 	
 
 	file.append("\n")
-	file.append(buildList.get(0).get("AppID") + (buildList.get(0).get("AppName"))
+	file.append(buildList.get(0).get("AppID") + (buildList.get(0).get("AppName")))
 	}
 	
 	
