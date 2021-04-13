537d8281457ae6eaec1bc9a0bb6a28c928373b26
kgmahendran <mahivalarmathi1@gmail.com>
2021-04-13 20:18:15 +0530

    Mahendran Final

--- vars/CSVpipeline.groovy
+++ vars/CSVpipeline.groovy
@@ -1,10 +1,8 @@
 #!/usr/bin/env groovy
 import org.apache.commons.csv.*
 import org.jenkinsci.plugins.*
-import java.util.stream.*
 
 def call() {
-
 	def records = readCSV file: 'Input.csv' , format: CSVFormat.DEFAULT.withHeader().withFirstRecordAsHeader()
 	def result = records.groupBy({record -> record.get("AppID")+"-"+record.get("AppName")+"-"+record.get("ReleaseVersion")})
 
@@ -24,19 +22,16 @@ def call() {
 		def  buildList = entry.getValue();
 		PassedList= buildList.findAll{f -> !f.get("Status").contains("Failed")}
 		FailedList =  buildList.findAll{it.Status.contains("Failed")}
-
 		if (PassedList != null && PassedList.size() > 0) {
 			def arrr = PassedList.collect{it.Environment}
 			passedEnvList = arrr.join('|')
 			Comments="Passed";
-		}
-		
+		}	
 		if (FailedList != null && FailedList.size() > 0) {
 			def arrr1 = FailedList.collect{it.Environment}
 			failedEnvList = arrr1.join('|')
 			Comments="Failed";
 		}
-
 		file.append("\n")
 		file.append(buildList.get(0).get("AppID") + Spliter + (buildList.get(0).get("AppName")) + Spliter + buildList.get(0).get("ReleaseVersion") + Spliter + passedEnvList + Spliter +failedEnvList + Spliter + Comments)
 	}
