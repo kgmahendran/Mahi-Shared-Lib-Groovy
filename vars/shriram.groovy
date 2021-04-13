2735a6bea7db29d5a51515f6e2eca7e1109677ab
kgmahendran <mahivalarmathi1@gmail.com>
2021-04-13 14:58:50 +0530

    test

--- vars/shriram.groovy
+++ vars/shriram.groovy
@@ -27,7 +27,7 @@ for (Map.Entry<String, List<CSVRecord>> entry : result.entrySet()) {
 
 	def FinalList=entry.getKey();
 	
-	FailedList=FinalList.findAll({record1 -> record1.getValue("Status").contains("Failed")})
+	FailedList=FinalList.findAll({record1 -> record1.getAt("Status").contains("Failed")})
 	
 	println "$FailedList"
 	}
