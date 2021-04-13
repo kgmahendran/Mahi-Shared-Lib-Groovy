49d64c022e8c659c0d19b84c34a36b23e28a6096
kgmahendran <mahivalarmathi1@gmail.com>
2021-04-13 11:20:19 +0530

    Test

--- vars/shriram.groovy
+++ vars/shriram.groovy
@@ -1,13 +1,8 @@
 #!/usr/bin/env groovy
 def call() {
 
-def CSVcontent = readCSV file: 'Input.csv', format: CSVFormat.DEFAULT.withHeader()
-
-//CSVParser parser = CSVFormat.EXCEL.parse(CSVcontent);
-
-//def str=CSVcontent.split(',')
-
-//def APPID=str[0]
+def excelFormat = CSVFormat.EXCEL
+def CSVcontent = readCSV file: 'Input.csv', format: excelFormat
 
 println "$CSVcontent"
   }
