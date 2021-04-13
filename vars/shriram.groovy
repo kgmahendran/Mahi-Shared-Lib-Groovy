e5d4643a804ec4c88ee8f3970d694c9dd7d69e6a
kgmahendran <mahivalarmathi1@gmail.com>
2021-04-13 18:11:09 +0530

    Test

--- vars/shriram.groovy
+++ vars/shriram.groovy
@@ -5,7 +5,7 @@ import java.util.stream.*
 
 def call() {
 
-
+def Headers="AppID,AppName,ReleaseVersion,EnvironmentsPassed,EnvironmentFailed,Comments"
 def records = readCSV file: 'D:\\Demo-Pipeline\\CSV-Jenkins\\Input.csv' , format: CSVFormat.DEFAULT.withHeader().withFirstRecordAsHeader()
 
 records.each { key,value ->
@@ -21,6 +21,9 @@ println "++++++++++++++++++++++++++++++++++++++++"
 result.each { key,value ->
 		println "$key : $value"
 }
+
+	File file = new File("D:\\Demo-Pipeline\\CSV-Jenkins\\output.csv")
+	file.append(Headers)
 
 println "********************************"
 List<CSVRecord> PassedList;
@@ -53,6 +56,7 @@ for (Map.Entry<String, List<CSVRecord>> entry : result.entrySet()) {
 	//println "+++++++++++++++++++++++++++"
 	
 	File file = new File("D:\\Demo-Pipeline\\CSV-Jenkins\\output.csv")
+	file.append(\n)
 	file.append(buildList.get(0).get("AppID"))
 	}
 	
