a278d8086385302de4648119bd3a8138396c8378
kgmahendran <mahivalarmathi1@gmail.com>
2021-04-13 11:05:05 +0530

    test

--- vars/shriram.groovy
+++ vars/shriram.groovy
@@ -1,8 +1,7 @@
 #!/usr/bin/env groovy
-import java.io.FileReader.*;
 def call() {
 
-Reader CSVcontent = new FileReader("D:\\Demo-Pipeline\\CSV-Jenkins\\Input.csv");
+def CSVcontent = readCSV file: 'Input.csv'''
 
 def str=CSVcontent.split(',')
 
