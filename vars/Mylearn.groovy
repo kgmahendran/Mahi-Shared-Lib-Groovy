61411004e056a1ee62056bf285beaec42901627a
kgmahendran <60134726+kgmahendran@users.noreply.github.com>
2021-04-23 14:49:31 +0530

    Create Mylearn.groovy

---
+++ vars/Mylearn.groovy
@@ -0,0 +1,25 @@
+def call(){
+
+		def inputJson = readJSON file: 'ARMTem.json'
+		inputJson.resources.each{it.put("condition","false")}
+		inputJson.resources.name.each{println it}
+		def Read_Deploy_project = readProperties file: 'Deploy_List.properties'
+		def KEY_NAME = Read_Deploy_project.keySet();
+		for (def i=0;i<KEY_NAME.size();i++)
+			{
+			def KEY_VALUES = Read_Deploy_project["${KEY_NAME[i]}"].split(',')
+			for (def j=0;j<=KEY_VALUES.size();j++)
+				{
+				inputJson.resources.each{
+					//println (it.name("${KEY_NAME[j]}")
+					if (it.name.contains("${KEY_VALUES[j]}") && it.type.contains("${KEY_NAME[i]}"))
+						{
+						echo "Test"
+						it.put("condition","true")
+						}
+					}
+				}
+			}
+			writeJSON file: 'ARMTem1.json', json: inputJson, pretty: 5;
+
+}
